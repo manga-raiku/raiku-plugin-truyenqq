0e9379ce59f56ec720a222c3f45658dfba127298
Tachibana Shin <tachibshin@duck.com>
2023-10-02 17:19:36 +0000

    fix test

--- test/setup-file.ts
+++ test/setup-file.ts
@@ -1,12 +1,3 @@
 import { parseDom } from "raiku-pgs/thread"
 
 Object.assign(self, { parseDom })
-const DR = Date
-Object.assign(self, {
-  Date: class Date extends DR {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    constructor(t: any) {
-      super(typeof t === "number" ? t : t + " GMT+0")
-    }
-  }
-})
