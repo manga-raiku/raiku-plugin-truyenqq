20001e5f42033feffdccab9e4a7ff9a7ee5d9169
Tachibana Shin <tachibshin@duck.com>
2023-10-04 09:47:29 +0700

    change index

--- src/runs/index.ts
+++ src/runs/index.ts
@@ -1,10 +1,16 @@
 import { CURL } from "../const"
 import Parse from "../parsers/index"
+import BangXepHang from "./bang-xep-bang/[type]"
 
 export default async function index() {
-  const { data } = await get({
-    url: CURL
-  })
+  const [index, topDay] = await Promise.all([
+    get({ url: CURL }).then((res) => Parse(data, Date.now())),
+    BangXepHang("/top-ngay", 1, {})
+  ])
 
-  return Parse(data, Date.now())
+  return {
+    sliders: topDay.items.slice(0, 7),
+    hot: topDay.items.slice(7),
+    last_update: index.last_update
+  }
 }
